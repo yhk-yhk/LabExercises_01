0d823d1194f6b3b5a49c788c04ee582531379fdb
yhk-yhk <56509442+yhk-yhk@users.noreply.github.com>
2019-11-19 09:17:52 +0800

    Create GradeBook.h

---
+++ 201816040101/Lab2/GradeBook.h
@@ -0,0 +1,22 @@
+// Lab 2: GradeBook.h
+// Definition of GradeBook class that stores an instructor's name.
+#include <string> // program uses C++ standard string class
+using namespace std;
+
+// GradeBook class definition
+class GradeBook
+{
+public:
+   // constructor initializes course name and instructor name
+   GradeBook( string, string );
+   void setCourseName( string ); // function to set the course name
+   string getCourseName(); // function to retrieve the course name
+   /* write code to declare a get function for the instructor's name */
+   void setInstructorName(string);
+   /* write code to declare a set function for the instructor's name */
+   string getInstructorName();
+   void displayMessage(); // display welcome message and instructor name
+private:
+   string courseName; // course name for this GradeBook
+   string instructorName; // instructor name for this GradeBook
+}; // end class GradeBook
